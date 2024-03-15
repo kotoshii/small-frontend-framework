b3f6769861c9b66cb6a0feb8138f236503c0d5ae
pusuwa <40234487+kotoshii@users.noreply.github.com>
2024-03-15 23:16:56 +0200

    Allow passing fragments as children.

--- packages/runtime/src/types/SFFElement.ts
+++ packages/runtime/src/types/SFFElement.ts
@@ -1,8 +1,10 @@
 import { VDOMNodeElement } from '~types/vdom/VDOMNodeElement';
+import { VDOMNodeFragment } from '~types/vdom/VDOMNodeFragment';
 
 // Values that can be passed as children
 export type SFFElement =
   | VDOMNodeElement
+  | VDOMNodeFragment
   | string
   | number
   | null
