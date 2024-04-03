89ce65547fd109139932cf19432724a0bebffcc4
pusuwa <40234487+kotoshii@users.noreply.github.com>
2024-04-03 21:12:56 +0300

    Rework getStore function. Expose new Store instance via it.

--- packages/runtime/index.ts
+++ packages/runtime/index.ts
@@ -1,7 +1,6 @@
 export { fragment, h } from '~core/h';
-export { getDispatcher } from '~core/hooks/get-dispatcher';
-export { getState } from '~core/hooks/get-state';
 export { createApp } from '~core/init/create-app';
+export { getStore } from '~core/state/global-state';
 export { SFFElement } from '~types/SFFElement';
 
 /*
--- packages/runtime/src/core/state/global-state.ts
+++ packages/runtime/src/core/state/global-state.ts
@@ -1,3 +1,6 @@
+import { Dispatcher } from '~core/state/dispatcher';
+import { StateActionHandler } from '~types/state/StateActionHandler';
+
 export class GlobalState {
   private static _instance: GlobalState | null = null;
   private _state: any = null;
@@ -25,4 +28,49 @@ export class GlobalState {
   set(state: unknown) {
     this._state = state;
   }
+}
+
+class Store {
+  private static _instance: Store | null = null;
+  private _globalState: GlobalState;
+  private _dispatcher: Dispatcher;
+
+  constructor() {
+    const globalState = GlobalState.instance();
+    const dispatcher = Dispatcher.instance();
+
+    if (!globalState) {
+      throw new Error('Cannot use store before initialization');
+    }
+    if (!dispatcher) {
+      throw new Error('Cannot use dispatcher before initialization');
+    }
+
+    this._globalState = globalState;
+    this._dispatcher = dispatcher;
+  }
+
+  static instance() {
+    if (!this._instance) {
+      this._instance = new Store();
+    }
+
+    return this._instance;
+  }
+
+  get data() {
+    return this._globalState.get();
+  }
+
+  dispatch<T>(action: string, payload: T) {
+    this._dispatcher.dispatch(action, payload);
+  }
+
+  subscribe(action: string, handler: StateActionHandler) {
+    return this._dispatcher.subscribe(action, handler);
+  }
+}
+
+export function getStore() {
+  return Store.instance();
 }
