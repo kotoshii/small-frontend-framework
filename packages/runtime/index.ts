a7d1b548150edd7f2ea8a0b8440685d8094b88e2
pusuwa <40234487+kotoshii@users.noreply.github.com>
2024-04-03 21:11:07 +0300

    Add todo item.

--- packages/runtime/index.ts
+++ packages/runtime/index.ts
@@ -3,3 +3,12 @@ export { getDispatcher } from '~core/hooks/get-dispatcher';
 export { getState } from '~core/hooks/get-state';
 export { createApp } from '~core/init/create-app';
 export { SFFElement } from '~types/SFFElement';
+
+/*
+ * TODO:
+ *   1. Local state support
+ *   2. Lifecycle methods
+ *   3. Router
+ *   4. Improve typings
+ *
+ * */
