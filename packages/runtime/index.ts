00528117a0269a1ab672898f4dc67db86c75058b
pusuwa <40234487+kotoshii@users.noreply.github.com>
2024-04-17 20:20:06 +0300

    Update todo

--- packages/runtime/index.ts
+++ packages/runtime/index.ts
@@ -1,12 +1,13 @@
-export { Component } from '~core/component';
+export { Component } from '~core/components/component';
 export { fragment, h } from '~core/h';
 export { createApp } from '~core/init/create-app';
 export { SFFElement } from '~types/vdom/SFFElement';
 
 /*
  * TODO:
- *   1. Local state support
+ *   1. Class component full support
  *   2. Lifecycle methods
+ *   2.1. Optimizations: re-render custom components only if props and state has changed (optional)
  *   3. Router
  *   4. Improve typings
  *
