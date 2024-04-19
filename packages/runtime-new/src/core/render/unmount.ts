1174c4350950d9c58c5c2794910c3e1f79f1fcc5
pusuwa <40234487+kotoshii@users.noreply.github.com>
2024-04-19 20:12:16 +0300

    Unmounting logic.

---
+++ packages/runtime-new/src/core/render/unmount.ts
@@ -0,0 +1,53 @@
+import { removeEventListeners } from '~core/render/events';
+import { VDOMNodeType } from '~core/vdom/constants/VDOMNodeType';
+import { SFFElement } from '~core/vdom/types/SFFElement';
+import {
+  VDOMNodeElement,
+  VDOMNodeFragment,
+  VDOMNodeText,
+} from '~core/vdom/types/VDOMNode';
+
+export function unmount(vnode: SFFElement) {
+  switch (vnode.type) {
+    case VDOMNodeType.ELEMENT: {
+      unmountElementNode(vnode);
+      break;
+    }
+    case VDOMNodeType.FRAGMENT: {
+      unmountFragmentNode(vnode);
+      break;
+    }
+    case VDOMNodeType.TEXT: {
+      unmountTextNode(vnode);
+      break;
+    }
+    default: {
+      throw new Error(`Couldn't unmount VDOM Node. Received: ${vnode}`);
+    }
+  }
+}
+
+function unmountElementNode(node: VDOMNodeElement) {
+  const { el, children, listeners } = node;
+
+  children.forEach(unmount);
+
+  if (el) {
+    removeEventListeners(listeners, el);
+    node.listeners = {};
+
+    el.remove();
+    node.el = null;
+  }
+}
+
+function unmountFragmentNode(node: VDOMNodeFragment) {
+  node.children.forEach(unmount);
+}
+
+function unmountTextNode(node: VDOMNodeText) {
+  if (node.el) {
+    node.el?.remove();
+    node.el = null;
+  }
+}
