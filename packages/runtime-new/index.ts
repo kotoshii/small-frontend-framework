ef07e9ed0f05528bfb010e95978fecff0c4def04
pusuwa <40234487+kotoshii@users.noreply.github.com>
2024-04-20 20:57:25 +0300

    Remove unused code, update TODO.

--- packages/runtime-new/index.ts
+++ packages/runtime-new/index.ts
@@ -1,45 +1,14 @@
 /*
  * TODO:
- *   1. Class component full support
- *   2. Lifecycle methods
- *   3. Optimizations: re-render custom components only if props and state has changed (optional)
- *   4. Router
- *   5. Improve typings
+ *   1. Global state
+ *   2. Router
+ *   3. Improve typings
+ *   4. CLI
  *
  * */
 
 import { Component } from '~core/components/component';
-import { mount } from '~core/render/mount';
-import { hComponent, hElement, hFragment } from '~core/vdom/h';
+import { createApp } from '~core/init/create-app';
+import { h, hFragment } from '~core/vdom/h';
 
-export { hElement, hFragment, mount };
-
-class Header extends Component {
-  render() {
-    return hElement('div', {}, [
-      hElement('p', {}, ['Home']),
-      hElement('p', {}, ['News']),
-      hElement('p', {}, ['About']),
-    ]);
-  }
-}
-
-class App extends Component {
-  render() {
-    return hElement('div', {}, hComponent(Header, { foo: 1 }, ['Test child']));
-  }
-}
-
-const vnode = hComponent(App, {}, [
-  hFragment(['ffffff']),
-  hElement('h1', { style: { fontSize: '100px' } }, ['Hello!!!']),
-  hElement('p', {}, ['Some description text lalalaala!!!']),
-  hElement('div', {}, [
-    hElement('button', { on: { click: () => {} } }, ['Click me!']),
-  ]),
-  null,
-  'string',
-  123,
-]);
-
-console.dir(vnode, { depth: null });
+export { Component, createApp, hFragment as fragment, h };
