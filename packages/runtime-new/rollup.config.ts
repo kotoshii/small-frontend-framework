70c3c6b169bf2abe45084d2d86a0dd040c6022e6
pusuwa <40234487+kotoshii@users.noreply.github.com>
2024-04-20 15:56:46 +0300

    Fix build.

--- packages/runtime-new/rollup.config.ts
+++ packages/runtime-new/rollup.config.ts
@@ -1,3 +1,4 @@
+import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
@@ -9,6 +10,7 @@ export default {
     typescript({
       compilerOptions: { declaration: false },
     }),
+    commonjs(),
     nodeResolve(),
     cleanup(),
   ],
