d8ae6a37c1259d4ab95f55d1ad67e0a1b66635d0
pusuwa <40234487+kotoshii@users.noreply.github.com>
2024-04-21 14:50:13 +0300

    Export additional types.

--- packages/runtime-new/index.ts
+++ packages/runtime-new/index.ts
@@ -10,5 +10,7 @@
 import { Component } from '~core/components/component';
 import { createApp } from '~core/init/create-app';
 import { h, hFragment } from '~core/vdom/h';
+import { SFFElement } from '~core/vdom/types/SFFElement';
+import { SFFNode } from '~core/vdom/types/SFFNode';
 
-export { Component, createApp, hFragment as fragment, h };
+export { Component, createApp, hFragment as fragment, h, SFFElement, SFFNode };
