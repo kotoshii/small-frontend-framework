b84ca3876e6a38f9c15ab092c08dc0c8d87c8b05
pusuwa <40234487+kotoshii@users.noreply.github.com>
2024-04-21 14:48:28 +0300

    EventBus implementation.

---
+++ packages/runtime-new/src/utils/event-bus/event-bus.ts
@@ -0,0 +1,39 @@
+import { EventBusListener } from '~utils/event-bus/types/EventBusListener';
+
+export class EventBus {
+  private static _instance: EventBus | null = null;
+  private readonly listeners: Record<string, EventBusListener[]> = {};
+
+  static create() {
+    if (this._instance) {
+      throw new Error(
+        'Attempt to create another EventBus instance per app. This behavior is abnormal, please check your code.',
+      );
+    }
+
+    this._instance = new EventBus();
+
+    return this._instance;
+  }
+
+  static instance() {
+    if (!this._instance) {
+      throw new Error('Cannot use EventBus before app initialization');
+    }
+
+    return this._instance;
+  }
+
+  on(eventName: string, listener: EventBusListener) {
+    if (!this.listeners[eventName]) {
+      this.listeners[eventName] = [];
+    }
+    this.listeners[eventName].push(listener);
+  }
+
+  emit(eventName: string, payload?: any) {
+    if (this.listeners[eventName]) {
+      this.listeners[eventName].forEach((listener) => listener(payload));
+    }
+  }
+}
