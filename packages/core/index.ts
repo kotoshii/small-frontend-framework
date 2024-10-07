3f35e1c0d6592f075d124913d8d1092ce51e5b0f
pusuwa <40234487+kotoshii@users.noreply.github.com>
2024-10-07 20:20:49 +0300

    feat(core): Add root index.ts file.

---
+++ packages/core/index.ts
@@ -0,0 +1,36 @@
+/*
+ * TODO:
+ *   1. rework building process (done)
+ *   2. add examples and docs
+ *   3. Improve typings (done)
+ *   4. Publish
+ *   5. CLI
+ *
+ * */
+
+import { Component } from '~core/components/component';
+import { createApp } from '~core/init/create-app';
+import { CreateAppOptions } from '~core/init/types/CreateAppOptions';
+import { Link } from '~core/router/components/link';
+import { Route } from '~core/router/components/route';
+import { Router } from '~core/router/components/router';
+import { ReducerFunction } from '~core/store/types/ReducerFunction';
+import { StateActionHandler } from '~core/store/types/StateActionHandler';
+import { h, hFragment as fragment } from '~core/vdom/h';
+import { SFFElement } from '~core/vdom/types/SFFElement';
+import { SFFNode } from '~core/vdom/types/SFFNode';
+
+export {
+  Component,
+  createApp,
+  CreateAppOptions,
+  fragment,
+  h,
+  Link,
+  ReducerFunction,
+  Route,
+  Router,
+  SFFElement,
+  SFFNode,
+  StateActionHandler,
+};
