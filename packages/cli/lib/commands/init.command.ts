31cb6d3d56809dc46d0c3775af6ad878ef1bd5d1
pusuwa <40234487+kotoshii@users.noreply.github.com>
2024-10-11 21:42:46 +0300

    feat(cli): Implement `init` command.

--- packages/cli/lib/commands/init.command.ts
+++ packages/cli/lib/commands/init.command.ts
@@ -1,3 +1,7 @@
+import chalk from 'chalk';
+import { ncp } from 'ncp';
+import { resolve } from 'path';
+
 import { AbstractCommand } from '~lib/commands/abstract.command';
 
 export class InitCommand extends AbstractCommand {
@@ -5,7 +9,32 @@ export class InitCommand extends AbstractCommand {
   description = 'Initialize a new project';
 
   async execute(path: string) {
-    // TODO: Add new project initialization logic.
-    console.log('`init` command', path);
+    if (path === '.') {
+      console.log('Initializing a new SFF project in the current folder...');
+    } else {
+      console.log(`Initializing a new SFF project at ${path}...`);
+    }
+
+    await this.copyInitTemplate(path);
+
+    console.log('Done!');
+    console.log('\nTo run the app:');
+    console.log(chalk.blue('npm install'));
+    console.log(chalk.blue('npm run start'));
+    console.log('\nTo build the app:');
+    console.log(chalk.blue('npm run build'));
+  }
+
+  copyInitTemplate(path: string) {
+    return new Promise((res, rej) => {
+      ncp(
+        resolve(__dirname, '../../templates/init'),
+        resolve(process.cwd(), path),
+        (err) => {
+          if (err) rej(err);
+          res(true);
+        },
+      );
+    });
   }
 }
